c57084f38e362db570b442df5707470bf97e4140
Stevens <stevenchauvets@gmail.com>
2025-06-15 18:03:27 +0200

    Retour en mode player tableau auto

--- js/app.js
+++ js/app.js
@@ -1,115 +1,23 @@
-let tab_ligue1 = {
-    equipe0: {
-        name: "Angers SCO",
-        but: scoreBut(),
-        logo: "https://www.footpack.fr/wp-content/uploads/2021/05/logo-angers-sco-868x1024.jpg"
-    }, 
-    equipe1: {
-        name: "AJ Auxerre",
-        but: scoreBut(),
-        logo: "https://logodownload.org/wp-content/uploads/2023/04/aj-auxerre-logo-0.png"
-    },
-    equipe2: {
-        name: "Stade Brestois 29",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe3: {
-        name: "Olympique de Marseille",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe4: {
-        name: "LOSC Lille",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe5: {
-        name: "Olympique Lyonnais",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe6: {
-        name:  "AS Monaco FC",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe7: {
-        name: "Montpellier Hérault SC",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe8: {
-        name: "FC Nantes",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe9: {
-        name: "Paris Saint-Germain",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe10: {
-    name: "OGC Nice",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe11: {
-        name: "Stade de Reims",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe12: {
-        name: "Stade Rennais FC",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe13: {
-        name: "AS Saint-Étienne",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe14: {
-        name:  "Toulouse FC",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe15: {
-        name: "RC Strasbourg Alsace",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe16: {
-        name: "Clermont Foot 63",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-    equipe17: {
-        name: "SM Caen",
-        but: scoreBut(),
-        logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/4/4c/Logo_SB29_2010.svg/1200px-Logo_SB29_2010.svg.png"
-    },
-}
+let tab_ligue1 = [ "Angers SCO", "AJ Auxerre", "Stade Brestois 29", "Le Havre AC", "RC Lens", "LOSC Lille", "Olympique Lyonnais", "Olympique de Marseille", "AS Monaco FC", "Montpellier Hérault SC", "FC Nantes", "OGC Nice", "Paris Saint-Germain", "Stade de Reims", "Stade Rennais FC", "AS Saint-Étienne", "Toulouse FC", "RC Strasbourg Alsace", "ES Troyes AC", "Clermont Foot 63", "SM Caen"];
 
-function cointMisor(number){ return number }
-function scoreBut(){ return Math.floor(Math.random() * 5); }
+const score = Math.floor(Math.random() * 8); // Score de 0 à 8.
+
 function operationPronostic(num){ return num * 2 }
 function autoPlayerFoot(num){ return Math.floor(Math.random() * num) }
 
 function playerFootPronostix(){
+    let tab = [];
     let numberPlayer = Number(prompt("Choisisser les équipes compris entre 0 et 10 qui joue cet periode !"));
         if(numberPlayer <= 10){
             for(let i = 0; i < operationPronostic(numberPlayer); i = i + 1){
-                console.log(Object.values(tab_ligue1))
-            }  
+                tab.push(tab_ligue1[autoPlayerFoot(tab_ligue1.length)])
+            }
+        console.log(tab)
         } else {
-                alert("Le nombre de joueurs est trop élevé pour le déroulement du pronostic !")
-                console.log('Le tournois est annulé : ERROR !')
-                false;  
+        alert("Le nombre de joueurs est trop élevé pour le déroulement du pronostic !")
+        console.log('Le tournois est annulé : ERROR !')
+        false;
         }
-        const myImage = new Image(100);
-        myImage.src = tab_ligue1.equipe0.logo;
-        document.body.appendChild(myImage);
     }
 
 playerFootPronostix()
